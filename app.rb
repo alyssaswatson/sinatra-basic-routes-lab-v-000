1a65369c2acd46913055a4d3e2953b6f8045e769
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-02-27 17:48:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,7 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
   get '/name' do
   "My name is Alyssa"
   end
+
+  get '/hometown' do
+  "My hometown is Knoxville"
+  end
+
+  get '/favorite-song' do
+  "My favorite song is xxx"
+  end
+
 end
